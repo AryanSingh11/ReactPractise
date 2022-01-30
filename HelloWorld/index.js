c503883db5a6727f32f01ab2f3d3885d118bf1a5
Aryan Singh <88196222+AryanSingh11@users.noreply.github.com>
2022-01-30 17:51:50 +0530

    Add files via upload

---
+++ HelloWorld/index.js
@@ -0,0 +1,10 @@
+
+ReactDOM.render(<h1>Hello World
+                 <ul>
+                     
+                   <li>React Developer</li>
+                   <li>Age 19</li>
+
+                 </ul>
+                 </h1>
+    , document.querySelector("#root"))
